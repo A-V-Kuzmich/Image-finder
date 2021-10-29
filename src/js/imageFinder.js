3be45399273d195f17ba3d6bfba7feb8cf7ec337
Dron010 <73987873+Dron010@users.noreply.github.com>
2021-10-29 09:51:43 +0300

    update

--- src/js/imageFinder.js
+++ src/js/imageFinder.js
@@ -25,19 +25,15 @@ function getImg(value) {
     fetchImg(value, page)
         .then(response => {
             console.log(response, page);
-            //return renderTpl(response.hits);
-            return response.hits;
+            renderTpl(response.hits);
         })
-        .then(()=>{page+=1});
-        
+        .then(()=>{page+=1});      
 };
 
 function renderTpl(imgCard) {
     console.log(imgCard);
-
-    gallery.insertAdjacentElement('beforeend', imgCardTpl(imgCard));
+    gallery.insertAdjacentHTML('beforeend', imgCardTpl(imgCard));
 };
-
 
 function loadMore() {
         getImg(qweryValue);
@@ -45,4 +41,5 @@ function loadMore() {
 
 function resetPage() {
     page = 1;
+    gallery.innerHTML = '';
  };
