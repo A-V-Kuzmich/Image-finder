e1fa8722777c67cbf0424e24156ac2280a53b2ba
Dron010 <73987873+Dron010@users.noreply.github.com>
2021-10-29 14:17:26 +0300

    add
    добавил стилизацию и отображение stats

--- src/js/imageFinder.js
+++ src/js/imageFinder.js
@@ -2,15 +2,13 @@ import { debounce } from "debounce";
 import { fetchImg } from './apiService.js';
 import imgCardTpl from '../templates/imgCard.hbs'
 
-let page = 1;
 const timeGet = 1000;
-
+let page = 1;
 let qweryValue = '';
 
 const gallery = document.querySelector('.gallery');
 const input = document.querySelector('#search-form');
 const button = document.querySelector('[data-action="add"]');
-
 
 button.addEventListener('click', () => {
     loadMore();
@@ -18,7 +16,7 @@ button.addEventListener('click', () => {
 input.addEventListener('input', debounce((e) => {
     qweryValue = e.target.value;
     getImg(qweryValue);
-    resetPage();
+    clearGallery();
 }, timeGet));
 
 function getImg(value) {
@@ -29,17 +27,14 @@ function getImg(value) {
         })
         .then(()=>{page+=1});      
 };
-
 function renderTpl(imgCard) {
     console.log(imgCard);
     gallery.insertAdjacentHTML('beforeend', imgCardTpl(imgCard));
 };
-
 function loadMore() {
         getImg(qweryValue);
 };
-
-function resetPage() {
+function clearGallery() {
     page = 1;
     gallery.innerHTML = '';
- };
+};
