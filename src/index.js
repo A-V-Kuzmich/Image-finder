e642b0ceb2dd652370a29c147fb4420146265b04
A-V-Kuzmich <a.v.kuzmi4@gmail.com>
2022-06-29 20:58:17 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -5,14 +5,3 @@ import './js/imageFinder.js';
 import './js/observer.js';
 import './js/modal.js';
 import './js/scroll.js';
-
-// setTimeout(
-//   () =>
-//     document
-//       .querySelector('meta[name=viewport]')
-//       .setAttribute(
-//         'content',
-//         'height=' + screen.height * 0.9 + 'px, width=device-width, initial-scale=1.0',
-//       ),
-//   300,
-// );
